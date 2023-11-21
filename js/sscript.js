5c064343c60bfba35dfdd4e79c32c6006d6cd42f
berkay <oziierdem59@gmail.com>
2023-11-21 12:17:30 +0300

    mesaj

--- js/sscript.js
+++ js/sscript.js
@@ -1,42 +1,40 @@
 // fetch('https://fakestoreapi.com/products')
-//             .then(res=>res.json())
-//             .then(json=>console.log(json))
+//     .then(res => res.json())
+//     .then(json => console.log(json))
 
-//! async await 
+// async await 
 
 const row = document.querySelector('.row')
-// console.log(row);
+let sepet =[]
+
+
 
 
 async function fetchData() {
     try {
-    let response = await fetch('https://api.escuelajs.co/api/v1/products')
-    let data = await response.json()
-    return data
-} catch (error) {
-    console.log(error);
-
+        let response = await fetch('https://fakestoreapi.com/products')
+        let data = await response.json()
+        return data
+    } catch (error) {
+        console.log(error);
     }
 }
 
 fetchData()
     .then(data => {
-        console.log(data);
+        console.log(data)
 
         // for (let i = 0; i < data.length; i++) {
         //     console.log(data[i]);
             
         // }
-        let sepet = []
 
+        
 
+        data.forEach(ürün => {
+            // console.log(ürün.image);
 
-
-
-        data.forEach((urun) => {
-            // console.log(urun.title);
-
-            const col = document.createElement('div')
+            const col =document.createElement('div')
             col.classList.add('col')
             col.style.width = '250px'
             // col.style.height = '400px'
@@ -46,60 +44,87 @@ fetchData()
             card.style.height = '100%'
             card.style.border = '1px solid black'
 
-
             const imgDiv = document.createElement('div')
-            imgDiv.style.width = '100%'
-            imgDiv.style.height = '250px'
+            
+            imgDiv.style.width ='100%'
+            imgDiv.style.height ='250px'
 
             const img = document.createElement('img')
-            img.src = urun.category.image
+            img.src = ürün.image
 
             img.style.width = '100%'
             img.style.height = '100%'
-
 
             const cardBody = document.createElement('div')
             cardBody.style.width = '100%'
             cardBody.style.height = '250px'
 
             const baslik = document.createElement('h5')
-            baslik.textContent = urun.title
+            baslik.textContent = ürün.title
 
             const aciklama = document.createElement('p')
-            aciklama.textContent = `${urun.description} - ${urun.price}$`
+            aciklama.textContent = `${ürün.price}$`
 
             const btn = document.createElement('button')
-            btn.classList.add('btn', 'btn-warning')
-            btn.textContent = 'Sepete Ekle'
+            btn.classList.add('btn','btn-warning')
+            btn.textContent = 'sepete ekle'
 
-            btn.addEventListener('click', () => {
-                console.log(urun);
 
-                sepet.push(urun)
+            btn.addEventListener('click',() => {
+                console.log(ürün);
+                sepet.push(ürün)
+
+                sepet.forEach (ürün => {
+                    const ürünAdi = document.createElement('p')
+                    ürünAdi.textContent = ürün.title
+                
+                    cart.append(ürünAdi)
+                })
+
+                let sepetJSON = JSON.stringify(sepet)
+                console.log(sepetJSON);
+
+                localStorage.setItem('sepet',sepetJSON)
 
                 console.log(sepet);
             })
 
-            
-
             cardBody.append(baslik)
             cardBody.append(aciklama)
             cardBody.append(btn)
-            
-            
             imgDiv.append(img)
+
             card.append(imgDiv)
             card.append(cardBody)
-
-
-
-
-
+            
+            
+            
+            
+            
             col.append(card)
-            
+
             row.append(col)
-            
         });
+    })
+
+
+    //! sepete ekleme 
+
+const cartIcon = document.querySelector('.fa-cart-shopping')
+
+const cart = document.querySelector('#sepet')
+
+cartIcon.addEventListener('click', () => {
+    cart.classList.toggle('aktif')
 
 })
 
+let localSepet = localStorage.getItem('sepet')
+let normalsepet = JSON.parse(localSepet)
+
+normalsepet.forEach(ürün => {
+    const baslik = document.createElement ('p')
+    baslik.textContent = ürün.title
+
+    cart.append(baslik)
+})
